8280b8927aaed3c889da3b1093c1a618fd55eb6b
Lopaka Delp <lopaka@readaccess.com>
2011-05-31 14:37:54 -0700

    fix syntax bug

--- cookbooks/db_mongodb/recipes/default.rb
+++ cookbooks/db_mongodb/recipes/default.rb
@@ -1,11 +1,13 @@
-remote_file "/tmp/mongodb-linux-i686-1.8.1.tgz" do
-    source "http://fastdl.mongodb.org/linux/mongodb-linux-i686-1.8.1.tgz"
+tarball = "mongodb-linux-i686-1.8.1.tgz"
+
+remote_file '/tmp/${tarball}' do
+    source "http://fastdl.mongodb.org/linux/#{tarball}"
     mode "0644"
 end
 
 execute "tar" do
-    cwd /tmp
-    command "tar xzf /tmp/mongodb-linux-i686-1.8.1.tgz"
+    cwd "/tmp"
+    command "tar xzf /tmp/#{tarball}"
     action :run
 end
 
