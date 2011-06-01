8ec3e1ae0bd16bee1ba35c0f8d2a043ffe1ae25b
Lopaka Delp <lopaka@readaccess.com>
2011-05-31 21:39:12 -0700

    no longer untars, just downloads

--- cookbooks/db_mongodb/recipes/default.rb
+++ cookbooks/db_mongodb/recipes/default.rb
@@ -1,12 +1,12 @@
 #--------------------
-# default recipe will download and untar monogodb into /tmp dir
+# default recipe will download monogodb into /tmp dir
 #--------------------
 
-tarball = "mongodb-linux-i686-1.8.1.tgz"
+tarball = "${node[:db_mongodb][:tarball]}"
 
 # appears to be a bug in using cookbook_file in chef 0.8
 # going back to wget
-##### cookbook_file "/tmp/mongodb-linux-i686-1.8.1.tgz" do
+##### cookbook_file "/tmp/#{tarball}" do
 #####     backup false
 #####     source "mongodb-linux-i686-1.8.1.tgz"
 ##### end
@@ -15,13 +15,6 @@ execute "wget" do
     cwd "/tmp"
     command "wget -q http://fastdl.mongodb.org/linux/#{tarball}"
     creates "/tmp/#{tarball}"
-    action :run
-end
-
-
-execute "tar" do
-    cwd "/tmp"
-    command "tar xzf /tmp/#{tarball}"
     action :run
 end
 
