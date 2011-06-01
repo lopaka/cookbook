532fff149a3d02bd12891f055bc680735176bc53
Lopaka Delp <lopaka@readaccess.com>
2011-06-01 04:19:28 -0700

    added more attributes

--- cookbooks/db_mongodb/recipes/default.rb
+++ cookbooks/db_mongodb/recipes/default.rb
@@ -4,13 +4,6 @@
 
 tarball = "#{node[:db_mongodb][:tarball]}"
 
-# appears to be a bug in using cookbook_file in chef 0.8
-# going back to wget
-##### cookbook_file "/tmp/#{tarball}" do
-#####     backup false
-#####     source "mongodb-linux-i686-1.8.1.tgz"
-##### end
-
 execute "wget" do
     cwd "/tmp"
     command "wget -q http://fastdl.mongodb.org/linux/#{tarball}"
