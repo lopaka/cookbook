3bd82b3bd2322d882b777ae49742f1ff4252b1ed
Lopaka Delp <lopaka@readaccess.com>
2011-05-31 18:39:37 -0700

    use chef resources

--- cookbooks/db_mongodb/recipes/install_mongodb.rb
+++ cookbooks/db_mongodb/recipes/install_mongodb.rb
@@ -1,8 +1,79 @@
 #--------------------
 # install_mongodb recipe will install mongodb to /usr/local/bin
+# and create need dirs and init scripts
 #--------------------
 
 content_dir = "/tmp/mongodb-linux-i686-1.8.1"
-`cp #{content_dir}/bin/* /usr/local/bin/`
-`rm -rf #{content_dir}`
 
+data_dir = "/var/mongodb/data"
+conf_file = "/etc/mongod.conf"
+log_file = "/var/log/mongod.log"
+
+
+#--------------------
+# copy content of bin
+#--------------------
+
+`cp #{content_dir}/bin/* /usr/local/bin/`
+
+
+#--------------------
+# remove content_dir
+#--------------------
+
+directory "#{content_dir}" do
+    recursive true
+    action :delete
+end
+
+#--------------------
+# create data dir
+#--------------------
+
+directory "#{data_dir}" do
+    owner "root"
+    group "root"
+    mode "0700"
+    recursive true
+    action :create
+end
+
+
+#--------------------
+# create simple conf file if it does not already exists
+#--------------------
+
+file "#{conf_file}" do
+    owner "root"
+    group "root"
+    mode "0755"
+    content "# #{conf_file} file"
+    not_if {File.exists?("#{conf_file}")}
+    action :create
+end
+
+
+#--------------------
+# create start up script
+#--------------------
+
+template "/etc/init.d/mongodb" do
+    source "init-mysql.erb"
+    mode 0755
+    owner "root"
+    group "root"
+    variables(
+        :data_dir => #{data_dir},
+        :conf_file => #{conf_file},
+        :log_file => #{log_file}
+    )
+end
+
+
+#--------------------
+# start the service
+#--------------------
+
+#service "mongod" go
+#    action :start
+#end
