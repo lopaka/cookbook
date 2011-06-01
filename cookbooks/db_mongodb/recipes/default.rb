945f0c41dcd897c45d4accf798cbef8285debf35
Lopaka Delp <lopaka@readaccess.com>
2011-05-31 19:21:25 -0700

    using cookbook_file instead of downloading

--- cookbooks/db_mongodb/recipes/default.rb
+++ cookbooks/db_mongodb/recipes/default.rb
@@ -4,12 +4,17 @@
 
 tarball = "mongodb-linux-i686-1.8.1.tgz"
 
-execute "wget" do
-    cwd "/tmp"
-    command "wget -q http://fastdl.mongodb.org/linux/#{tarball}"
-    creates "/tmp/#{tarball}"
-    action :run
+cookbook_file "/tmp/#{tarball}"
+    backup false
+    source "#{tarball}
 end
+
+#execute "wget" do
+#    cwd "/tmp"
+#    command "wget -q http://fastdl.mongodb.org/linux/#{tarball}"
+#    creates "/tmp/#{tarball}"
+#    action :run
+#end
 
 
 execute "tar" do
