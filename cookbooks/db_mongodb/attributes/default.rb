682ec05dea9181f0ece81e43603f2cda740f38f4
Lopaka Delp <lopaka@readaccess.com>
2011-05-31 21:39:37 -0700

    added tarball as an attribute

--- cookbooks/db_mongodb/attributes/default.rb
+++ cookbooks/db_mongodb/attributes/default.rb
@@ -1,3 +1,4 @@
 default[:db_mongodb][:data_dir] = "/var/mongodb/data"
 default[:db_mongodb][:conf_file] = "/etc/mongod.conf"
 default[:db_mongodb][:log_file] = "/var/log/mongod.log"
+default[:db_mongodb][:tarball] = "mongodb-linux-i686-1.8.1.tgz"
