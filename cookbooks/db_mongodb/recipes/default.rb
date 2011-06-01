44ad727402e6ea5c50b34a70f12da65e26653666
Lopaka Delp <lopaka@readaccess.com>
2011-05-31 19:46:05 -0700

    back to using wget

--- cookbooks/db_mongodb/recipes/default.rb
+++ cookbooks/db_mongodb/recipes/default.rb
@@ -4,10 +4,20 @@
 
 tarball = "mongodb-linux-i686-1.8.1.tgz"
 
-cookbook_file "/tmp/mongodb-linux-i686-1.8.1.tgz" do
-    backup false
-    source "mongodb-linux-i686-1.8.1.tgz"
+# appears to be a bug in using cookbook_file in chef 0.8
+# going back to wget
+##### cookbook_file "/tmp/mongodb-linux-i686-1.8.1.tgz" do
+#####     backup false
+#####     source "mongodb-linux-i686-1.8.1.tgz"
+##### end
+
+execute "wget" do
+    cwd "/tmp"
+    command "wget -q http://fastdl.mongodb.org/linux/#{tarball}"
+    creates "/tmp/#{tarball}"
+    action :run
 end
+
 
 execute "tar" do
     cwd "/tmp"
