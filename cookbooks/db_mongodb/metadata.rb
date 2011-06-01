923ee136dc6ae2bfca11c7d8aefae442c24dc943
Lopaka Delp <lopaka@readaccess.com>
2011-06-01 04:42:24 -0700

    default will do whole process

--- cookbooks/db_mongodb/metadata.rb
+++ cookbooks/db_mongodb/metadata.rb
@@ -5,6 +5,4 @@ description      "Installs/configures a MongoDB server."
 long_description "Installs/configures a MongoDB server."
 version          "0.0.1"
 
-recipe  "db_mongodb::default", "downloads and untars mongodb."
-recipe  "db_mongodb::install_mongodb", "installs mongodb into /usr/local/bin/"
-recipe  "db_mongodb::clean_source", "cleans source files"
+recipe  "db_mongodb::default", "installs mongodb"
