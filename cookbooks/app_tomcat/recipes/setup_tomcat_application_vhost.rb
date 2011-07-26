67ff8322d5d865012ebb748ca0af6fbbdd8c5416
Lopaka Delp <lopaka@rightscale.com>
2011-07-25 22:18:28 -0700

    [app_tomcat] default recipe no longer edits tomcat config files - moved to setup_tomcat_application_vhosts

---
+++ cookbooks/app_tomcat/recipes/setup_tomcat_application_vhost.rb
@@ -0,0 +1,22 @@
+case node[:platform]
+when "centos","fedora","suse"
+ 
+ # edit files /etc/tomcat6/tomcat6.conf and /etc/tomcat6/server.xml
+  # replace port 8080 with port 8000
+  tomcat_conf_files = [ '/etc/tomcat6/tomcat6.conf', '/etc/tomcat6/server.xml' ]
+  tomcat_conf_files.each do |conf_file|
+    file_text = File.read(conf_file)
+    file_text.gsub(/8080/, "8000")
+    file conf_file do
+        owner "root"
+        group "root"
+        mode "0644"
+        action :create
+        backup 5
+        content file_text
+    end
+  end
+else
+    log "nothing done yet for non centos"
+end
+
