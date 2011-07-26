74bc1d619f5543fde8b4b2d8bc988704075a9278
Lopaka Delp <lopaka@rightscale.com>
2011-07-26 09:20:26 -0700

    [app_tomcat] setting config files to use port 8000 vs 8080

--- cookbooks/app_tomcat/recipes/setup_tomcat_application_vhost.rb
+++ cookbooks/app_tomcat/recipes/setup_tomcat_application_vhost.rb
@@ -1,26 +1,15 @@
-#case node[:platform]
-#when "centos","fedora","suse"
-# 
-#  # edit files /etc/tomcat6/tomcat6.conf and /etc/tomcat6/server.xml
-#  # replace port 8080 with port 8000
-#  tomcat_conf_files = [ '/etc/tomcat6/tomcat6.conf', '/etc/tomcat6/server.xml' ]
-#  tomcat_conf_files.each do |conf_file|
-#    if (File.exists?(conf_file))
-#      file_text = File.read(conf_file)
-#      file_text.gsub(/8080/, "8000")
-#      file conf_file do
-#          owner "root"
-#          group "root"
-#          mode "0644"
-#          action :create
-#          backup 5
-#          content file_text
-#      end
-#    else
-#      log "#{conf_file} does not exist"
-#    end
-#  end
-#else
-#    log "nothing done yet for non centos"
-#end
+template "/etc/tomcat6/tomcat6.conf" do
+  action :create
+  source "tomcat6_conf.erb"
+  group "root"
+  owner "root"
+  mode "0644"
+end
 
+template "/etc/tomcat6/server.xml" do
+  action :create
+  source "server_xml.erb"
+  group "root"
+  owner "root"
+  mode "0644"
+end
