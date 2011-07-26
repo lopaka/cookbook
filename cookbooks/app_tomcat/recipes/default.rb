8875c28817603f768aa36e5f90b1b8ce2c08c01c
Lopaka Delp <lopaka@rightscale.com>
2011-07-25 21:27:06 -0700

    [app_tomcat] alters config files from port 8080 to port 8000

--- cookbooks/app_tomcat/recipes/default.rb
+++ cookbooks/app_tomcat/recipes/default.rb
@@ -65,7 +65,31 @@ when "centos","fedora","suse"
     end
   end
 
+  # edit files /etc/tomcat6/tomcat6.conf and /etc/tomcat6/server.xml
+  # replace port 8080 with port 8000
+  
+  tomcat_conf_files = [ '/etc/tomcat6/tomcat6.conf', '/etc/tomcat6/server.xml' ]
+  tomcat_conf_files.each do |conf_file|
+    file_text = File.read(conf_file)
+    puts file_text.gsub(/8080/, "8000")
+    file conf_file do
+        owner "root"
+        group "root"
+        mode "0644"
+        action :create
+        backup 5
+        content file_text
+    end
+  end
 
+
+  #--------------------
+  # start tomcat6
+  #--------------------
+
+  #service tomcat6 start
+  #
+  #
 else
     log "nothing done yet for non centos"
 end
