00454d6621be0ad7a09e3ea896e1332955ca832b
Lopaka Delp <lopaka@rightscale.com>
2011-07-26 21:40:51 -0700

    [app_tomcat] server template development

--- cookbooks/app_tomcat/recipes/setup_db_connection.rb
+++ cookbooks/app_tomcat/recipes/setup_db_connection.rb
@@ -6,8 +6,8 @@
 
 template "/etc/tomcat6/context.xml" do
   source "context_xml.erb"
-  owner
-  group
+  owner "root"
+  group "root"
   mode "0644"
   variables(
     :user      => node[:db_mysql][:application][:user],
@@ -17,3 +17,30 @@ template "/etc/tomcat6/context.xml" do
   )
 end
 
+template "/etc/tomcat6/web.xml" do
+  source "web_xml.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+template "/usr/share/tomcat6/webapps/ROOT/dbread.jsp" do
+  source "dbread_jsp.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+cookbook_file "/usr/share/tomcat6/lib/jstl-api-1.2.jar" do
+  source "jstl-api-1.2.jar"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+cookbook_file "/usr/share/tomcat6/lib/jstl-impl-1.2.jar" do
+  source "jstl-impl-1.2.jar"
+  owner "root"
+  group "root"
+  mode "0644"
+end
