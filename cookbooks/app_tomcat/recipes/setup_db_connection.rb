50389909af639b5fbb2fa95e446db64954827281
Lopaka Delp <lopaka@rightscale.com>
2011-07-26 21:19:37 -0700

    [app_tomcat] server template development

--- cookbooks/app_tomcat/recipes/setup_db_connection.rb
+++ cookbooks/app_tomcat/recipes/setup_db_connection.rb
@@ -13,7 +13,7 @@ template "/etc/tomcat6/context.xml" do
     :user      => node[:db_mysql][:application][:user],
     :ipassword => node[:db_mysql][:application][:password],
     :fqdn      => node[:db_mysql][:fqdn],
-    :database  => node[:app_tomcat][:db_name]
+    :database  => node[:tomcat][:db_name]
   )
 end
 
