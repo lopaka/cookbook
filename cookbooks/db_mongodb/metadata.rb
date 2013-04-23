fe0a6018b81d5d847ee21fbe6a39b95a2274dfc1
Lopaka Delp <lopaka@rightscale.com>
2013-04-23 12:43:03 -0700

    [db_mongodb] rake metadata

--- cookbooks/db_mongodb/metadata.rb
+++ cookbooks/db_mongodb/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Lopaka Delp"
 maintainer_email "lopaka@readaccess.com"
 license          'Apache v2.0'
 description      "Installs/configures a MongoDB server."
-long_description "Installs/configures a MongoDB server."
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 recipe  "db_mongodb::default", "installs mongodb"
